fee69731980fd892b5dbccaabfeadf806dce4de3
Melina Alejos <malejos1219@gmail.com>
2020-06-23 10:40:17 -0400

    Add new mapping for New and POST method in controller

--- src/main/java/com/win/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/win/techtalentblog/BlogPost/BlogPostController.java
@@ -1,5 +1,8 @@
 package com.win.techtalentblog.BlogPost;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,17 +11,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlogPostController {
+
     @Autowired
     private BlogPostRepository blogPostRepository;
 
-    @GetMapping
-    public String index(BlogPost blogPost) {
+    private static List<BlogPost> posts = new ArrayList<>();
+
+    @GetMapping(value = "/")
+    public String index(BlogPost blogPost, Model model) {
+        model.addAttribute("posts", posts);
         return "index";
     }
 
     private BlogPost blogPost;
 
-    @PostMapping
+    @GetMapping(value = "new")
+    public String index(BlogPost blogPost) {
+        return "new";
+    }
+
+    @PostMapping(value = "/blogposts")
     private String addNewBlogPost(BlogPost blogPost, Model model) {
         blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
         model.addAttribute("title", blogPost.getTitle());
