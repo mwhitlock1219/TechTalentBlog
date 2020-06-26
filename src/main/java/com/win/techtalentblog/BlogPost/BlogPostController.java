871382d0753f2d8ba437b0d2e04d0d39eabb920f
Melina Alejos <malejos1219@gmail.com>
2020-06-26 09:18:20 -0400

    Part of Goal 3: added edit button to posts

--- src/main/java/com/win/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/win/techtalentblog/BlogPost/BlogPostController.java
@@ -2,12 +2,16 @@ package com.win.techtalentblog.BlogPost;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BlogPostController {
@@ -48,5 +52,15 @@ public class BlogPostController {
         model.addAttribute("author", blogPost.getAuthor());
         model.addAttribute("blogEntry", blogPost.getBlogEntry());
         return "blogpost/result";
+    }
+
+    @RequestMapping(value = "/blogposts/{id}", method = RequestMethod.GET)
+    public String editPostWithId(@PathVariable Long id, BlogPost blogPost, Model model) {
+        Optional<BlogPost> post = blogPostRepository.findById(id);
+        if (post.isPresent()) {
+            BlogPost actualPost = post.get();
+            model.addAttribute("blogPost", actualPost);
+        }
+        return "blogpost/edit";
     }
 }
