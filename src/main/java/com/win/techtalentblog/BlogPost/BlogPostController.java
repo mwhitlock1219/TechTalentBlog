a44abbe7bf3c42837b16e2fa0c0087a905afc6dd
Melina Alejos <malejos1219@gmail.com>
2020-06-26 11:57:21 -0400

    Completed optional tasks: Add confirmation modal and edit button in results page

--- src/main/java/com/win/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/win/techtalentblog/BlogPost/BlogPostController.java
@@ -40,7 +40,7 @@ public class BlogPostController {
 
     @PostMapping(value = "/blogposts")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
-        blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+        blogPostRepository.save(blogPost);
         model.addAttribute("blogPost", blogPost);
         // model.addAttribute("title", blogPost.getTitle());
         // model.addAttribute("author", blogPost.getAuthor());
